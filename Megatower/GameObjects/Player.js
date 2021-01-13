3203571f31b16f894439b55e658de3e72107d12c
AntonyA3 <antonyademefun@gmail.com>
2021-01-13 17:00:10 +0000

    created physics objects

--- Megatower/GameObjects/Player.js
+++ Megatower/GameObjects/Player.js
@@ -2,13 +2,13 @@ class Player {
   constructor() {
     this.position = Vector3.zero();
     this.velocity = Vector3.zero();
-    this.box = new AA_Box(0,0,0,32,64,32);
+    this.sphere = new Sphere(new Vector3(16,16,16),16);
     this.sprite = new Sprite3D();
 
   }
 
   moveBy(offset){
-    this.box = this.box.offset(offset);
+    this.sphere = this.sphere.offset(offset);
     this.position = Vector3.add(this.position,offset);
     this.sprite.moveBy(offset);
   }
