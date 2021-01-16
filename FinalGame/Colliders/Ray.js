bc403ab02552733f67b1f1aacd3ceb811422c0e3
AntonyA3 <antonyademefun@gmail.com>
2021-01-16 20:43:29 +0000

    manifold generator written

--- FinalGame/Colliders/Ray.js
+++
@@ -1,6 +0,0 @@
-class Ray {
-  constructor(position, direction) {
-    this.position = position || Vector3.zero();
-    this.direction = direction || Vector3.zero();
-  }
-}
--- FinalGame/Colliders/Rect.js
+++
@@ -1,8 +0,0 @@
-class Rect {
-  constructor(x,y,sx,sy) {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.sx = sx || 0;
-    this.sy = sy || 0;
-  }
-}
