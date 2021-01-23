73ba39a02c89e0070c5ad7c0bc8ac772bec2d328
AntonyA3 <antonyademefun@gmail.com>
2021-01-23 16:16:24 +0000

    Redo the game

--- Megatower/Colliders/Capsule.js
+++
@@ -1,8 +0,0 @@
-class Capsule {
-  constructor(min, max, radius) {
-    this.min = min || Vector3.zero();
-    this.max = max || Vector3.zero();
-    this.radius = radius || 0;
-
-  }
-}
--- Megatower/GameObjects/Texture.js
+++
@@ -1,7 +0,0 @@
-class Texture {
-  constructor(src) {
-    this.image = new Image();
-    this.image.onload = function(){};
-    this.image.src = src;
-  }
-}
